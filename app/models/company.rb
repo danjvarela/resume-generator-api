9ec0a76cb2474f014e880db66e2da6ebe52d1f97
Dan <vareladanmarj@gmail.com>
2023-02-16 00:35:35 +0800

    fix: preven't ransacking company id

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,6 +3,6 @@ class Company < ApplicationRecord
   validates :name, presence: true, uniqueness: {case_sensitive: false}
 
   def self.ransackable_attributes(auth_object = nil)
-    ["id", "name"]
+    ["name"]
   end
 end
