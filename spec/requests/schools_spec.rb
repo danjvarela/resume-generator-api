b7f5cd94dc83b8149b61e7c014b1bd6c64785797
pazs49 <percival49tv@gmail.com>
2023-02-20 19:31:32 +0800

    test: add schools_spec

--- spec/requests/schools_spec.rb
+++ spec/requests/schools_spec.rb
@@ -1,7 +1,34 @@
-require 'rails_helper'
+require "rails_helper"
+require "contexts/authenticate_user"
+require "examples/response_examples"
 
 RSpec.describe "Schools", type: :request do
+  include_context "sign in user"
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before :all do
+      3.times { create :school }
+      get schools_path, **@auth_headers
+    end
+
+    include_examples "response status", 200
+
+    it "should return all schools" do
+      expect(json_body).to eq format_to_response(School.all)
+    end
+  end
+
+  describe "GET /search" do
+    before :all do
+      3.times { create :school }
+      @search_params = {name_cont: "a"}
+      get schools_search_path, params: {q: @search_params}, **@auth_headers
+    end
+
+    include_examples "response status", 200
+
+    it "should return searched schools" do
+      expect(json_body).to eq format_to_response(School.ransack(@search_params).result(distinct: true))
+    end
   end
 end
