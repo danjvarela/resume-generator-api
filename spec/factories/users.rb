693e548aa6335d558ac60a0471592171592051d1
pazs49 <percival49tv@gmail.com>
2023-03-03 12:24:12 +0800

    fix: use factory bot in generating users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     name { generate :user_name }
     email { Faker::Internet.email name: name }
     password { "password" }
+    phone_number { Faker::PhoneNumber.cell_phone }
+    address { Faker::Address.full_address }
     confirmed_at { Time.now }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  subject { build :user }
   it { should have_many(:jobs).dependent(:destroy) }
   it { should have_many(:skills).through(:user_skills) }
   it { should have_many(:resumes).dependent(:destroy) }
